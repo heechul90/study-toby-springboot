c98077fb37eb77993ca12bda1f578d445fcd7c87
hclee <heechul4296@gmail.com>
2023-02-14 22:52:27 +0900

    new sourcce

--- src/main/java/study/spring/springboot/HelloController.java
+++ src/main/java/study/spring/springboot/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(String name) {
-        return "Hello " + name;
+        return "Hello " + name + "!!!";
     }
 }
