d0805a8ed9165c857527d7ceb5e5aaf8d7788448
hclee <heechul4296@gmail.com>
2023-02-14 22:33:37 +0900

    new sourcce

---
+++ src/main/java/study/spring/springboot/HelloController.java
@@ -0,0 +1,13 @@
+package study.spring.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(String name) {
+        return "Hello " + name;
+    }
+}
