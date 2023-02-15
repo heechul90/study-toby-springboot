3f822be7979cae16cb4f2a0772bcdc3a40a4a5c4
hclee <heechul4296@gmail.com>
2023-02-15 21:48:57 +0900

    Hello 컨트롤러 매핑과 바인딩

--- src/main/java/study/spring/springboot/HelloController.java
+++ src/main/java/study/spring/springboot/HelloController.java
@@ -1,13 +1,8 @@
 package study.spring.springboot;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HelloController {
 
-    @GetMapping("/hello")
     public String hello(String name) {
-        return "Hello " + name + "!!! test";
+        return "Hello " + name + "!!!";
     }
 }
--- src/main/java/study/spring/springboot/SpringbootApplication.java
+++ src/main/java/study/spring/springboot/SpringbootApplication.java
@@ -21,6 +21,9 @@ public class SpringbootApplication {
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            HelloController helloController = new HelloController();
+
+
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -28,14 +31,16 @@ public class SpringbootApplication {
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        String ret = helloController.hello(name);
+
                         //상태코드
                         resp.setStatus(HttpStatus.OK.value());
                         //해더
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
                         //바디
-                        resp.getWriter().println("Hello " + name);
+                        resp.getWriter().println(ret);
                     } else if (req.getRequestURI().equals("/user")) {
-
+                        //
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
