46b24e2022f65e33c85658a3ae7cacfee0734a0d
hclee <heechul4296@gmail.com>
2023-02-15 21:25:27 +0900

    서블릿 등록

--- src/main/java/study/spring/springboot/SpringbootApplication.java
+++ src/main/java/study/spring/springboot/SpringbootApplication.java
@@ -2,14 +2,36 @@ package study.spring.springboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SpringbootApplication {
 
     public static void main(String[] args) {
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    //상태코드
+                    resp.setStatus(200);
+                    //해더
+                    resp.setHeader("Content-Type", "text/plain");
+                    //바디
+                    resp.getWriter().println("Hello Servlet");
+
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
+
 
 
 
