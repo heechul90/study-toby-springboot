7890b6bbab022bceaf4bbf5b0a3b9c3a95f1440e
hclee <heechul4296@gmail.com>
2023-02-16 22:23:08 +0900

    애노테이션 매핑 정보 사용

--- src/main/java/study/spring/springboot/HelloController.java
+++ src/main/java/study/spring/springboot/HelloController.java
@@ -1,7 +1,12 @@
 package study.spring.springboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
+@RequestMapping
 public class HelloController {
 
     private final HelloService helloService;
@@ -10,6 +15,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping("/hello")
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
--- src/main/java/study/spring/springboot/SpringbootApplication.java
+++ src/main/java/study/spring/springboot/SpringbootApplication.java
@@ -2,22 +2,9 @@ package study.spring.springboot;
 
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 public class SpringbootApplication {
 
