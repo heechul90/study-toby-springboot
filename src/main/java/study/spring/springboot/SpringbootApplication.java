9a554b07e717efed8af95743c9d64c653c327921
hclee <heechul4296@gmail.com>
2023-02-16 21:42:50 +0900

    스프링 컨테이너 사용

--- src/main/java/study/spring/springboot/SpringbootApplication.java
+++ src/main/java/study/spring/springboot/SpringbootApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -19,24 +20,27 @@ import java.io.IOException;
 public class SpringbootApplication {
 
     public static void main(String[] args) {
+        //스프링 컨테이너
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
-
-
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
+            servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
                     //인증, 보안, 다국어, 공통 기능 등
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(name);
 
                         //상태코드
                         resp.setStatus(HttpStatus.OK.value());
                         //해더
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         //바디
                         resp.getWriter().println(ret);
                     } else if (req.getRequestURI().equals("/user")) {
