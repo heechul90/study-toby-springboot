f133455f88f8adfcc678af0be2cfb4c39f4d8a48
hclee <heechul4296@gmail.com>
2023-02-16 21:49:10 +0900

    의존 오브젝트 추가

--- src/main/java/study/spring/springboot/HelloController.java
+++ src/main/java/study/spring/springboot/HelloController.java
@@ -1,8 +1,11 @@
 package study.spring.springboot;
 
+import java.util.Objects;
+
 public class HelloController {
 
     public String hello(String name) {
-        return "Hello " + name + "!!!";
+        SimpleHelloService helloService = new SimpleHelloService();
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/study/spring/springboot/SimpleHelloService.java
@@ -0,0 +1,7 @@
+package study.spring.springboot;
+
+public class SimpleHelloService {
+    String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
