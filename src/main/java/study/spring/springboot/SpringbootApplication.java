8efc7b114acf003a23eafdf4c7b293eb15c1d6b1
hclee <heechul4296@gmail.com>
2023-02-16 23:07:05 +0900

    SpringBootApplication

--- src/main/java/study/spring/springboot/SpringbootApplication.java
+++ src/main/java/study/spring/springboot/SpringbootApplication.java
@@ -1,13 +1,11 @@
 package study.spring.springboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
@@ -25,23 +23,6 @@ public class SpringbootApplication {
     }
 
     public static void main(String[] args) {
-        //스프링 컨테이너 빈 등록
-        AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-            @Override
-            protected void onRefresh() {
-                super.onRefresh();
-
-                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-                WebServer webServer = serverFactory.getWebServer(servletContext -> {
-                    servletContext.addServlet("dispatcherServlet", dispatcherServlet)
-                            .addMapping("/*");
-                });
-                webServer.start();
-            }
-        };
-        applicationContext.register(SpringbootApplication.class);
-        applicationContext.refresh();
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 }
